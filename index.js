92dbec62f3317723731ef1e3586423ea9f5765b1
Ernesto Campohermoso <ecampohermoso@cirrus-it.net>
2021-08-19 20:24:30 -0400

    Implementados el POST y el GET de tareas

---
+++ index.js
@@ -0,0 +1,29 @@
+//https://stackoverflow.com/questions/9177049/express-js-req-body-undefined
+
+var express = require("express");
+var bodyParser = require('body-parser')
+
+var jsonParser = bodyParser.json()
+//var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+var app = express();
+
+var tasks = []
+
+app.get("/", (req, res, next) => {
+    res.json("{ 'message': 'Tasks server online'}");
+});
+
+app.post("/tasks", jsonParser, (req, res, next) => {
+    req.body.id = tasks.length + 1;
+    tasks.push(req.body);
+    res.send("OK");
+});
+
+app.get("/tasks", (req, res, next) => {
+    res.json(tasks);
+});
+
+app.listen(3000, () => {
+    console.log("Servidor HTTP funcionando");
+});
